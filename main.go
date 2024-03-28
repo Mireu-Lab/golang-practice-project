2256ff5971932b465b0435902658495c5e0e6c76
mireu-lab <limmireu1214@gmail.com>
2024-03-28 16:00:14 +0900

    update main.go and test.go

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 
 	"github.com/Mireu-Lab/golang-practice-project/greeting"
+	"github.com/Mireu-Lab/golang-practice-project/src"
 )
 
 func main() {
 	fmt.Println(greeting.HelloKo())
 	fmt.Println(greeting.HelloEn())
+
+	src.Asay()
 }
--- src/test.go
+++ src/test.go
@@ -1,4 +1,4 @@
-package src_test
+package src
 
 import "fmt"
 
