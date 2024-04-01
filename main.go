ca278eb01a744f04a50bb3204fa48390aeb1c303
mireu-lab <limmireu1214@gmail.com>
2024-04-01 13:30:40 +0900

    update main.go

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/Mireu-Lab/golang-practice-project/greeting"
-	"github.com/Mireu-Lab/golang-practice-project/greeting/en"
-	"github.com/Mireu-Lab/golang-practice-project/greeting/ko"
-	"github.com/Mireu-Lab/golang-practice-project/src"
-)
-
 func main() {
-	fmt.Println(greeting.HelloKo())
-	fmt.Println(greeting.HelloEn())
 
-	src.Asay()
-
-	fmt.Print(en.Hello())
-	fmt.Print(ko.Hello())
 }
